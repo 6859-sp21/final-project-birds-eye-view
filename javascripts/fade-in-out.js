bcbc415356597f502fd7ad5d452dc7b00458467b
Stacia Johanna <staciaj@mit.edu>
2021-05-01 16:29:45 -0400

    add transition section

--- javascripts/fade-in-out.js
+++ javascripts/fade-in-out.js
@@ -32,4 +32,24 @@ function checkCategoryDecadeValidity() {
         return;
     }
     warningDiv.innerText = "";
+    var questionDiv = document.getElementById('question-before-color');
+    var questionDescDiv = document.getElementById('question-description-before-color');
+    if (chosenCategory === "electricity") {
+        questionDiv.innerText = "How well do you know about the world's access to electricity in " + chosenDecade + "s?";
+    }
+    else if (chosenCategory === "internet") {
+        questionDiv.innerText = "How well do you know about the world's access to Internet in " + chosenDecade + "s?";
+    }
+    else if (chosenCategory === "school-completion") {
+        questionDiv.innerText = "How well do you know about the world's primary school completion in " + chosenDecade + "s?";
+    }
+    else if (chosenCategory === "skilled-birth") {
+        questionDiv.innerText = "How well do you know about the world's access to skilled health staff during birth in " + chosenDecade + "s?";
+    }
+    questionDescDiv.innerHTML = `
+    We all live different lifestyles of varying levels of abundance and access to different kinds of resources. 
+    <br>
+    While we are familiar with the resources available in our surroundings in this decade, 
+    it is time to see in bird's eye view on other countries' or other decades' access to different resources.`;
+    return;
 }
