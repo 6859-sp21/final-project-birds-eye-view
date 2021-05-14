3501a3577736fc1042863ec602f85d2afb3374f2
Stacia Johanna <staciaj@mit.edu>
2021-05-14 08:42:25 -0400

    clean up folders and convert to geojson

--- scripts/csv_to_geojson.py
+++ scripts/csv_to_geojson.py
@@ -4,13 +4,14 @@ from geojson import Feature, FeatureCollection, Point
 features = []
 with open('../data/electricity_decade_fin.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
-    for country, latitude, longitude, country_code,year,value in reader:
+    for country, region, latitude, longitude, country_code,year,value in reader:
         try:
             latitude, longitude = map(float, (latitude, longitude))
             features.append(
                 Feature(
                     geometry = Point((longitude, latitude)),
                     properties = {
+                        'region': region,
                         'country' : country,
                         'country_code' : country_code,
                         'year' : year,
