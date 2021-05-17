1e6238b0d8ef9e2371f94c9819e4051e9528cbc9
Stacia Johanna <staciaj@mit.edu>
2021-05-17 18:37:00 -0400

    Add template for stories

--- javascripts/fade-in-out.js
+++ javascripts/fade-in-out.js
@@ -32,16 +32,13 @@ function checkCategoryDecadeValidity() {
         return;
     }
     warningDiv.innerText = "";
-    var questionDiv = document.getElementById('question-before-color');
+    /*var questionDiv = document.getElementById('question-before-color');
     var questionDescDiv = document.getElementById('question-description-before-color');
     if (chosenCategory === "electricity") {
         questionDiv.innerText = "How well do you know about the world's access to electricity in " + chosenDecade + "s?";
     }
-    else if (chosenCategory === "internet") {
-        questionDiv.innerText = "How well do you know about the world's access to Internet in " + chosenDecade + "s?";
-    }
-    else if (chosenCategory === "school-completion") {
-        questionDiv.innerText = "How well do you know about the world's primary school completion in " + chosenDecade + "s?";
+    else if (chosenCategory === "urban-agglomerate") {
+        questionDiv.innerText = "How well do you know about the world's state of urban agglomeration in " + chosenDecade + "s?";
     }
     else if (chosenCategory === "skilled-birth") {
         questionDiv.innerText = "How well do you know about the world's access to skilled health staff during birth in " + chosenDecade + "s?";
@@ -50,6 +47,40 @@ function checkCategoryDecadeValidity() {
     We all live different lifestyles of varying levels of abundance and access to different kinds of resources. 
     <br>
     While we are familiar with the resources available in our surroundings in this decade, 
-    it is time to see in bird's eye view on other countries' or other decades' access to different resources.`;
+    it is time to see in bird's eye view on other countries' or other decades' access to different resources.`;*/
+
+    if (chosenCategory === "electricity") {
+        if (chosenDecade === "1990") {
+            document.getElementById("story-electricity-1990").style.visibility = "visible";
+        }
+        else if (chosenDecade === "2000") {
+            document.getElementById("story-electricity-2000").style.visibility = "visible";
+        }
+        else if (chosenDecade === "2010") {
+            document.getElementById("story-electricity-2010").style.visibility = "visible";
+        }
+    }
+    else if (chosenCategory === "skilled-birth") {
+        if (chosenDecade === "1990") {
+            document.getElementById("story-birth-1990").style.visibility = "visible";
+        }
+        else if (chosenDecade === "2000") {
+            document.getElementById("story-birth-2000").style.visibility = "visible";
+        }
+        else if (chosenDecade === "2010") {
+            document.getElementById("story-birth-2010").style.visibility = "visible";
+        }
+    }
+    else if (chosenCategory === "urban-agglomerate") {
+        if (chosenDecade === "1990") {
+            document.getElementById("story-urban-1990").style.visibility = "visible";
+        }
+        else if (chosenDecade === "2000") {
+            document.getElementById("story-urban-2000").style.visibility = "visible";
+        }
+        else if (chosenDecade === "2010") {
+            document.getElementById("story-urban-2010").style.visibility = "visible";
+        }
+    }
     return;
 }
