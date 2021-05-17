16f0a45f0635107fca9d2fd0fe3f9f6cde2d98f7
Stacia Johanna <staciaj@mit.edu>
2021-05-17 13:46:11 -0400

    finish urban agglomerate

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("../data/past-data/birth-2.csv")
+df = pd.read_csv("../data/past-data/urban-2.csv")
 
 #print(df.head(20))
 
@@ -11,4 +11,4 @@ df['decade'] = df['year'] // 10 * 10
 decade_means_df = df.groupby(['country', 'country_code', 'decade']).mean()
 del decade_means_df['year']
 
-decade_means_df.to_csv('../data/past-data/birth-3.csv')
+decade_means_df.to_csv('../data/past-data/urban-3.csv')
